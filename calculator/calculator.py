9ceeeca795377d810c69b266c117f41d18578986
YADAVALLI JAYANTH KUMAR <115489547+Jayanthkumar04@users.noreply.github.com>
2026-02-24 12:56:16 +0530

    file to github broo

---
+++ calculator/calculator.py
@@ -0,0 +1,17 @@
+#calculator.py
+
+def add(a, b):
+    """Returns sum of a and b"""
+    return a + b
+
+def subtract(a, b):
+    """Returns difference between a and b"""
+    return a - b
+
+class Calculator:
+    def multiply(self, a, b):
+        return a * b
+
+if __name__ == "__main__":
+    calc = Calculato()
+    print(calc.multiply(3, 4))
